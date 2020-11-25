13b62a962d1cb2ce68e43f6025bdd339d23fb50f
Erik <erikadarisman@gmail.com>
2020-11-25 15:09:50 +0700

    add env n nodemon n add config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [TodosModule],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
