091056d6abf85e85c4cfd90a7563ca85b0f5dca2
Erik <erikadarisman@gmail.com>
2020-11-25 15:20:45 +0700

    update entity todos

--- src/todos/entities/todo.entity.ts
+++ src/todos/entities/todo.entity.ts
@@ -1,1 +1,15 @@
-export class Todo {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export abstract class Todo {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 300 })
+  title: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'boolean', default: false })
+  isDone: boolean;
+}
