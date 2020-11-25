afd2cd99fb5dfb7082cdc216888edf7e34de11af
Erik <erikadarisman@gmail.com>
2020-11-25 14:51:29 +0700

    add resource todos and add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Todos example')
+    .setDescription('The Todo API description')
+    .setVersion('1.0')
+    .addTag('documentation')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('documentation', app, document);
   await app.listen(3000);
 }
 bootstrap();
